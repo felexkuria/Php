37e25062bd9e36243c58d278216be5c169476a83
felex <engineerfelex@gmail.com>
2021-11-25 22:44:04 +0300

    String functions

--- index.php
+++ index.php
@@ -21,12 +21,26 @@
 </head>
 
 <body>
-<!-- <div class="card-panel  teal lighten-2">This is a card panel with a teal lighten-2 class</div> -->
-            
+    <!-- <div class="card-panel  teal lighten-2">This is a card panel with a teal lighten-2 class</div> -->
+
 
     <?php
+    $name = "Felex";
+
     echo "<h6>Hello World</h6>";
+    echo strtolower($name);
     echo "<br>";
+
+    echo strlen($name);
+    echo "<br>";
+    echo strtoupper($name);
+    echo "<br>";
+
+    echo $name[0];
+    echo "<br>";
+    echo str_replace("Felex", "Virginia", $name);
+    echo "<br>";
+    echo substr($name, 5, 2);
     echo "<h6 class=\"blue-text text-darken-2\">Me From Africa</h6>";
 
 
